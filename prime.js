4587cdc85a6cf845c3e69bb4d28c8bbbb300f331
VikramKalra05 <vikramkalra2005@gmail.com>
2023-08-27 20:45:20 +0530

    added fixed code

--- prime.js
+++ prime.js
@@ -1,15 +1,16 @@
 // Check whether number is prime or not
 
 let num = 10;
+let sqroot = num*0.5;
 let factors = 0;
 
-for(let i=0; i<=num; i++){
+for(let i=2; i<sqroot; i++){
     if(num%i==0){
         factors++;
     }
 }
 
-if(factors=2){
+if(factors==2){
     console.log("prime")
 }else{
     console.log("not prime");
